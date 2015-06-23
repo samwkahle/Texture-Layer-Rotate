03e74e7b8c8f2eec2221b699e184b6663c9746bd
Sam Kahle <samwkahle@users.noreply.github.com>
2015-06-22 22:22:44 -0400

    Create LayerRotation.cs

---
+++ LayerRotation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class LayerRotation : MonoBehaviour {
+
+	public float planetSpeedRotation = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate(Vector3.up * Time.deltaTime * planetSpeedRotation);
+
+
+	}
+}
